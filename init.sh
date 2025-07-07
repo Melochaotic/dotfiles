cd0b2c0d67a44639018359fa17459cf2c77fad15
Nicko J. Ruddock <nicholas.ruddock@taxassist.co.uk>
2025-07-07 19:15:05 +0100

    ➕ install `pnpm`

--- init.sh
+++ init.sh
@@ -42,3 +42,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 source ~/.zshrc
 # nvm install --lts && nvm alias default lts/*
 nvm install 24 && nvm alias default 24
+
+npm install -g pnpm@latest-10
