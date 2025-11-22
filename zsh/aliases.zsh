ed40f9b3f0b7cf3a643b64c402349ddba69d7264
Nicko J. Ruddock <nicholas.ruddock@taxassist.co.uk>
2025-11-22 20:56:02 +0000

    ✨ Install `yazi`

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -110,3 +110,12 @@ boop () {
   fi
   $(exit "$last")
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
